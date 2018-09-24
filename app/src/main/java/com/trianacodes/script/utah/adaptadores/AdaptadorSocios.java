42bb35cadbc6b7504d4da0de210259db9f9ddc68
Worome <41673409+Worome@users.noreply.github.com>
2018-09-24 13:27:45 +0200

    Inicio

--- app/src/main/java/com/trianacodes/script/utah/adaptadores/AdaptadorSocios.java
+++ app/src/main/java/com/trianacodes/script/utah/adaptadores/AdaptadorSocios.java
@@ -1,13 +1,31 @@
 package com.trianacodes.script.utah.adaptadores;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.trianacodes.script.utah.Entidades.Socios;
+import com.trianacodes.script.utah.R;
+
+import java.util.ArrayList;
 
 public class AdaptadorSocios extends RecyclerView.Adapter<AdaptadorSocios.SociosViewHolder> {
+
+    ArrayList<Socios> listaSocios;
+
+    public AdaptadorSocios(ArrayList<Socios> listaSocios) {
+        this.listaSocios = listaSocios;
+    }
+
     @Override
     public SociosViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, null, false);
+
+        return new SociosViewHolder(view);
     }
 
     @Override
@@ -21,8 +39,16 @@ public class AdaptadorSocios extends RecyclerView.Adapter<AdaptadorSocios.Socios
     }
 
     public class SociosViewHolder extends RecyclerView.ViewHolder{
+
+        TextView txtNombre, txtNick, txtCargo;
+        ImageView ivFoto;
+
         public SociosViewHolder(View itemView) {
             super(itemView);
+            txtNombre = itemView.findViewById(R.id.txtNombre);
+            txtNick = itemView.findViewById(R.id.txtNick);
+            txtCargo = itemView.findViewById(R.id.txtCargo);
+            ivFoto = itemView.findViewById(R.id.ivFoto);
         }
     }
 }
